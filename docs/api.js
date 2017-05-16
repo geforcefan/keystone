edaeb5512395fb50c001af5288463bfc8579b5e4
Ercan Akyürek <ercan.akyuerek@gmail.com>
2017-05-16 21:47:36 +0200

    docs

---
+++ docs/api.js
@@ -0,0 +1,38 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "server.ServerApplication",
+        "server.definitions.errorCodes",
+        "server.definitions.mailTypes",
+        "server.definitions.permissions",
+        "server.helper.AbstractSchedule",
+        "server.helper.Mailer",
+        "server.helper.Router",
+        "server.helper.RouterMethods",
+        "server.helper.Scheduler",
+        "server.helper.Service",
+        "server.helper.Services",
+        "server.helper.Setup",
+        "server.helper.auth",
+        "server.helper.errorResponse",
+        "server.helper.mongoose",
+        "server.helper.permissions",
+        "server.helper.successResponse",
+        "server.models.FailedMailSchema",
+        "server.models.SocialLinksSchema",
+        "server.models.SystemSchema",
+        "server.models.UserGroupSchema",
+        "server.models.UserProfileSchema",
+        "server.models.UserSchema",
+        "server.routes.User",
+        "server.schedules.FailedMails",
+        "server.services.User",
+        "server.services.UserProfile",
+        "shared.definitions.languages",
+        "shared.helper.i18n"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
